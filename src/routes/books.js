a39906971b5d717b80b6d945f4063aecdb565ef0
Chiien <fuchienhsu12@gmail.com>
2018-05-22 15:49:43 -0300

    Update url params and delete sqlite

--- src/routes/books.js
+++ src/routes/books.js
@@ -4,7 +4,7 @@ const books = (server) => {
 
     server.get(`/books`, BooksController.getAllBooks.bind(BooksController))
     server.post(`/books/book`, BooksController.postBook.bind(BooksController))
-    server.get(`/books/by-title/:name`, BooksController.getBookByTitle.bind(BooksController))
+    server.get(`/books/by-title/:title`, BooksController.getBookByTitle.bind(BooksController))
 }
 
 module.exports = books
