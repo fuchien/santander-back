68ff2f02e39b44b79e1f3f3507905e62e100100c
Chiien <fuchienhsu12@gmail.com>
2018-05-22 00:10:15 -0300

    Add clients models

---
+++ src/models/clients/clients.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataType) => {
+
+    const Clients = sequelize.define('Clients', {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome: {
+            type: DataType.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        age: {
+            type: DataType.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        photo: {
+            type: DataType.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        }
+    })
+
+    return Clients
+}
