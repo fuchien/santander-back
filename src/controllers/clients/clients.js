edeba5e777c693f0b9f9d98b7a17d6851cf88742
Chiien <fuchienhsu12@gmail.com>
2018-05-22 10:03:20 -0300

    Add service to do query and call in controllers

--- src/controllers/clients/clients.js
+++ src/controllers/clients/clients.js
@@ -1,33 +1,26 @@
-const app = require('../../server')
-const Clients = app.datasource.models.Clients
+const clientsService = require('../../services/clients/clients.service')()
 
 function ClientsController () {
 
-    this.getData = (req, res, next) => {
-        Clients.findAll()
-        .then(clients => {
-            console.log(clients)
+    this.getData = async (req, res, next) => {
+
+        try {
+            let clients = await clientsService.getAllData()
             res.status(200).json(clients)
-        })
-        .catch(err => {
+        } catch (err) {
             res.status(400).json(err)
-        })
+        }
     }
 
-    this.postData = (req, res, next) => {
+    this.postData = async (req, res, next) => {
         const data = req.body
         console.log(data)
-        Clients.create(data)
-        .then(client => {
-            console.log(client)
-            console.log(client.dataValues)
-            res.status(200).json({
-                client: client.dataValues,
-                msg: 'Salvo com sucesso!'
-            })
-        }).catch(err => {
+        try {
+            let client = await clientsService.postData(data)
+            res.status(200).json(client.data)
+        } catch (err) {
             res.status(400).json(err)
-        })
+        }        
     }
 }
 
---
+++ src/services/clients/clients.service.js
@@ -0,0 +1,38 @@
+const app = require('../../server')
+const Clients = app.datasource.models.Clients
+
+function ClientsService () {
+
+    this.postData = (data) => {
+
+        return new Promise((resolve, reject) => {
+
+            Clients.create(data)
+                .then(client => {
+                    console.log(client)
+                    resolve({
+                        data: client.dataValues,
+                        msg: 'Success to save!'
+                    })
+                }).catch(err => {
+                    reject(err)
+                })
+        })
+    }
+
+    this.getAllData = () => {
+
+        return new Promise((resolve, reject) => {
+
+            Clients.findAll()
+                .then(clients => {
+                    resolve(clients)
+                })
+                .catch(err => {
+                    reject(err)
+                })
+        })
+    }
+}
+
+module.exports = () => new ClientsService()
