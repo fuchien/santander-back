daa855ded28c52ebfcb1dc5a801936fbafdadbfb
Chiien <fuchienhsu12@gmail.com>
2018-05-22 14:17:37 -0300

    Bug fix to post and get data

--- src/controllers/clients/clients.js
+++ src/controllers/clients/clients.js
@@ -2,27 +2,27 @@ const clientsService = require('../../services/clients/clients.service')()
 
 function ClientsController () {
 
-    this.getData = async (req, res, next) => {
+    this.getAllClients = async (req, res, next) => {
         try {
-            let clients = await clientsService.getAllData()
+            let clients = await clientsService.getAllClients()
             res.status(200).json(clients)
         } catch (err) {
             res.status(400).json(err)
         }
     }
 
-    this.postData = async (req, res, next) => {
+    this.postClient = async (req, res, next) => {
         try {
-            let client = await clientsService.postData(req.body)
+            let client = await clientsService.postClient(req.body)
             res.status(200).json(client.datas)
         } catch (err) {
             res.status(400).json(err)
         }        
     }
 
-    this.getDataByName = async (req, res, next) => {
+    this.getClientByName = async (req, res, next) => {
         try {
-            let clients = await clientsService.getDataByName(req.params.name)
+            let clients = await clientsService.getClientByName(req.params.name)
             res.status(200).json(clients)
         } catch (err) {
             res.status(400).json(err)
