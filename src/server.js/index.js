d19599d96708694c2e97869dd64458a31647fddd
Chiien <fuchienhsu12@gmail.com>
2018-05-21 23:41:31 -0300

    Add CORS in project

--- src/server.js/index.js
+++ src/server.js/index.js
@@ -2,3 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Method", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
