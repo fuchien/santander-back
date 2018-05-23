51c351db0b71b4f1beec10197155ee7e2ffa243b
Chiien <fuchienhsu12@gmail.com>
2018-05-23 18:09:55 -0300

    Add unit test to Books postBook

--- tests/unit/books/books.js
+++ tests/unit/books/books.js
@@ -2,7 +2,7 @@ const BooksController = require('../../../src/controllers/books/books')()
 const BooksServices = require('../../../src/services/books/books.service')
 
 describe('Controllers: Books', () => {
-  describe('Get all books: getAll()', () => {
+  describe('Get all books: getAllBooks()', () => {
     it('should return a list of books', () => {
       const Books = {
         findAll: td.function(),
@@ -21,6 +21,40 @@ describe('Controllers: Books', () => {
       const booksServices = new BooksServices(Books);
 
       booksServices.getAllBooks()
+        .then(response => {
+          expect(response.data).to.be.eql(expectedResponse)
+        })
+    });
+  });
+
+  describe('POST book: postBook()', () => {
+    it('should save a book', () => {
+      const Books = {
+        create: td.function(),
+      };
+
+      const expectedResponse = [{
+        id: 1,
+        author: 'Test Book',
+        title: 'Test Book',
+        subtitle: 'Test Book',
+        created_at: '2016-08-06T23:55:36.692Z',
+        updated_at: '2016-08-06T23:55:36.692Z',
+      }];
+
+      const requestedResponse = {
+        id: 1,
+        author: 'Test Book',
+        title: 'Test Book',
+        subtitle: 'Test Book',
+        created_at: '2016-08-06T23:55:36.692Z',
+        updated_at: '2016-08-06T23:55:36.692Z',
+      };
+
+      td.when(Books.create(requestedResponse)).thenReturn(expectedResponse);
+      const booksServices = new BooksServices(Books);
+
+      booksServices.postBook(requestedResponse)
         .then(response => {
           expect(response.data).to.be.eql(expectedResponse)
         })
