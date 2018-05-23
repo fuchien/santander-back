013c558801b775dc074d568fd1f84ac0b0fa9b67
Chiien <fuchienhsu12@gmail.com>
2018-05-23 18:33:56 -0300

    Add unit test to Books getBookByTitle

--- tests/unit/books/books.js
+++ tests/unit/books/books.js
@@ -60,4 +60,29 @@ describe('Controllers: Books', () => {
         })
     });
   });
+
+  describe('Get book by title: getBookByTitle()', () => {
+    it('should return a book searched by Title', () => {
+      const Books = {
+        findAll: td.function(),
+      };
+
+      const expectedResponse = [{
+        id: 1,
+        author: 'Test Book',
+        title: 'Test Book',
+        subtitle: 'Test Book',
+        created_at: '2016-08-06T23:55:36.692Z',
+        updated_at: '2016-08-06T23:55:36.692Z',
+      }];
+
+      td.when(Books.findAll({where : {$like : `%Book%`}})).thenReturn(expectedResponse);
+      const booksServices = new BooksServices(Books);
+
+      booksServices.getBookByTitle(`Book`)
+        .then(response => {
+          expect(response.data).to.be.eql(expectedResponse)
+        })
+    });
+  });
 });
