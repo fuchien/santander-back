78911a93ade130b9c41b25eac2815d0b778c1e20
Chiien <fuchienhsu12@gmail.com>
2018-05-23 18:59:45 -0300

    Add reporter to show coverage

--- tests/unit/books/books.js
+++ tests/unit/books/books.js
@@ -85,4 +85,45 @@ describe('Controllers: Books', () => {
         })
     });
   });
+
+  describe('Get book by title: getBookByAuthor()', () => {
+    it('should return a book searched by Author', () => {
+      const Books = {
+        findAll: td.function(),
+      };
+
+      const expectedResponse = [{
+        id: 1,
+        author: 'Test Book',
+        title: 'Test Book',
+        subtitle: 'Test Book',
+        created_at: '2016-08-06T23:55:36.692Z',
+        updated_at: '2016-08-06T23:55:36.692Z',
+      }];
+
+      td.when(Books.findAll({where : {$like : `%Book%`}})).thenReturn(expectedResponse);
+      const booksServices = new BooksServices(Books);
+
+      booksServices.getBookByAuthor(`Book`)
+        .then(response => {
+          expect(response.data).to.be.eql(expectedResponse)
+        })
+    });
+    it('should return a book searched by Author Error', () => {
+      const Books = {
+        findAll: td.function(),
+      };
+
+      const errorMsg = `Error`
+
+      td.when(Books.findAll({where : {$like : `%Book%`}})).thenThrow(new Error(errorMsg));
+      const booksServices = new BooksServices(Books);
+
+      booksServices.getBookByAuthor(`Book`)
+        .then(err => {
+          console.log(`ERROR ---> `, err)
+          expect(err).to.be.eql(errorMsg)
+        })
+    });
+  });
 });
